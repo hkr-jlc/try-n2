f32de4ec804e5e573376474c914496eafc36a425
hkr-jlc <jlc27hikari@gmail.com>
2026-04-18 11:02:22 +0700

    Update app.js

--- app.js
+++ app.js
@@ -602,9 +602,8 @@ function renderEssay(essayData) {
     essayData.paragraphs.forEach((para, idx) => {
         html += `
             <p class="essay-paragraph">
-                <span class="sentence-jp">${highlightGrammar(para.jp)}</span>
+                <span class="sentence-jp">${highlightGrammar(para.jp)}</span></p>
                 ${para.id ? `<span class="translation-id">${para.id}</span>` : ''}
-            </p>
         `;
     });
     
